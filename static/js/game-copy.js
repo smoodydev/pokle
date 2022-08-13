3904375f2f8acf2e3d45b4c1a500f4836a99e32a
Stephen Moody <darilligames@gmail.com>
2022-08-13 18:50:48 +0000

    UI - Testing 'type cards' being shown to help show what types the user could still guess to help

---
+++ static/js/game-copy.js
@@ -0,0 +1,158 @@
+let pokemon_types = [
+    "Normal","Fire","Water","Grass","Electric","Ice","Fighting","Poison","Ground","Flying","Psychic","Bug","Rock","Ghost","Dark","Dragon","Steel","Fairy"
+]
+
+function get_pokemon() {
+    let the_pokemon = $(`#pokemon_entered`).val().toLowerCase();
+   
+    if (pokemonNames.includes(the_pokemon)) {
+        return the_pokemon;
+    }
+    return false;
+
+}
+
+function make_type_card(types, section) {
+  
+    for (let type in types) {
+        if (types[type] != "") {
+            if (!$("#" + section + " ." + types[type].toLowerCase()).length) {
+                $("#"+section).append(`<div class="typebadge ${types[type].toLowerCase()}">
+                ${types[type].toUpperCase()}
+            </div>`);
+            }
+
+        }
+    }
+
+
+}
+
+function make_attack_card(move, move_result){
+    return `<div class="c4 md1"><p>Used ${move_result.move_details[0]}</p>
+    <div class="effective effective-${move_result.val}">x${move_result.val}</div>
+    <div class="typebadge ${move_result.move_details[1]}">${move_result.move_details[1].toUpperCase()}
+    </div>`
+}
+
+function make_pokemon_card(the_pokemon) {
+    let height, weight = "";
+    if (the_pokemon.height[0]){
+        height = "Taller";
+        if (the_pokemon.height[1] > $("#tallerThan").text()){
+            min_height = the_pokemon.height[1];
+            $("#tallerThan").html(the_pokemon.height[1])
+        }
+       
+        
+    }else{
+        height = "Shorter"
+        if (the_pokemon.height[1] < $("#shorterThan").text()){
+            max_height = the_pokemon.height[1];
+            $("#shorterThan").html(the_pokemon.height[1])
+        }
+   
+        
+    }
+    
+    if (the_pokemon.weight[0]){
+        weight = "Heavier"
+        if (the_pokemon.weight[1] > $("#heavierThan").text()){
+            $("#heavierThan").html(the_pokemon.weight[1]);
+        }
+    }else{
+        weight = "Lighter"
+        if (the_pokemon.weight[1] < $("#lighterThan").text()){
+            $("#lighterThan").html(the_pokemon.weight[1]);
+        }
+        
+    }
+    return `
+    <div class="c4 md1 soften">
+        <img src="static/gifs/${the_pokemon.name.replaceAll(" ", "-").toLowerCase()}.gif"><p>${the_pokemon.name}</p><p>${height}<p><p>${weight}<p>
+    </div>`
+}
+
+function generateTypeCards(){
+    pokemon_types.forEach(function(type){
+        
+        $("#typeCards").append(`
+        <div id="type-card-${type}" class="typebadge ${type.toLowerCase()}">
+            ${type}
+        </div>`);
+        console.log(type);
+    });
+}
+generateTypeCards();
+
+function removeTypeCard(types){
+    types.forEach(function (type){
+        $("#type-card-"+type).remove();
+    })
+
+}
+
+function try_word() {
+    let the_pokemon = get_pokemon();
+    if (the_pokemon) {
+        $.post($SCRIPT_ROOT + '/guess_pokemon', {
+            pokemon: the_pokemon
+        }, function (data) {
+            if (data.validated) {
+                pk = data.result;
+                console.log(pk);
+                
+                // wordsent = "";
+                // paintCells(data.result, word_attempt);
+                // $("#result").text("Result for " + word_attempt);
+                // attempt++;
+                // letter_index = 0;
+                // attempt_array.push(data.result);
+                // if (data.result == "y".repeat(num_letters)){
+                //     alert("You are a Wiener!")
+                //     openWinModal();
+                // }
+                make_type_card(pk.types, "correctTypes");
+
+                removeTypeCard(pk.types.concat(pk.not_types));
+
+                // make_type_card(pk.not_types, "guessedTypes");
+                if (data.code == 2){
+                    $("#thepkimg").attr("src", "static/gifs/"+pk.name.toLowerCase()+".gif");
+                    $("#correctHeight").html("Height: "+pk.height+"m")
+                    $("#correctWeight").html("Weight: "+pk.weight+"kg")
+                    
+                }
+                else{
+                    $("#guessedPokemon").append(make_pokemon_card(pk));
+                }
+                
+                // $("#guessedTypes").append(make_type_card(pk.not_types));
+            }
+            else {
+                $("#result").text(data.text_back);
+            }
+        });
+    }
+}
+
+$('.usemove').bind('click', function () {
+    $.post($SCRIPT_ROOT + '/use_move', {
+            move_slot: this.id
+        }, function (data) {
+            if (data.validated) {
+                move_result = data.result;
+                $("#guessedPokemon").append(make_attack_card(this.innerhtml, move_result));
+            }
+            else {
+                $("#result").text(data.text_back);
+            }
+        });
+    return false;
+});
+
+
+$('#enter').bind('click', function () {
+    try_word();
+    return false;
+});
