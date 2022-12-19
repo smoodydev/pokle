bab590075d9ac12cd93ccb44ad60a43df9625320
Stephen Moody <darilligames@gmail.com>
2022-12-19 20:20:45 +0000

    Feature - Moves now usable

--- static/js/twitch-addon.js
+++ static/js/twitch-addon.js
@@ -2,14 +2,18 @@ const intervalID = setInterval(checkOption, 500);
 
 function checkOption() {
     console.log("Checking");
-    let option = $("#pokemonToGuess").text()
-    console.log(option);
-    if (option != ""){
+    let pkOption = $("#pokemonToGuess").text()
+    console.log(pkOption);
+    if (pkOption != ""){
         $("#pokemon_entered").val($("#pokemonToGuess").text());
         $("#pokemonToGuess").text("")
         $("button#enter").click()
     }
-    
+    let moveOption = $("#useMove").text()
+    if (moveOption != ""){
+        $("#useMove").text("");
+        $(".usemove#"+moveOption).click();
+    }
 
     
 }
